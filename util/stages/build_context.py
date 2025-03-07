43a055e9baf18e2bc0fa87707d98d80510a5ed56
justintzeji <justintzeji@gmail.com>
2025-03-07 16:05:30 +0800

    feat: iteration modules

--- util/stages/build_context.py
+++ util/stages/build_context.py
@@ -16,12 +16,12 @@ def prop_md(prop_dict):
 
 def example_block(comp_name, comp_examples, props):
     return (
-        f"\n\n# full code examples of React components that use {comp_name} :\n"
+        f"\n\n## full code examples of React components that use `{comp_name}` :\n"
         + "\n\n".join([f"```tsx\n{example.strip()}\n```" for example in comp_examples])
         + "\n\n"
-        f"# API reference for {comp_name} :\n\n"
+        f"## API reference for `{comp_name}` :\n"
         + "\n".join(
-            [f"## {c_name}\n\n{prop_md(c_prop)}" for c_name, c_prop in props.items()]
+            [f"\n### {c_name}\n\n{prop_md(c_prop)}" for c_name, c_prop in props.items()]
         )
     )
 
@@ -58,6 +58,35 @@ def gen_comp_task(prompt, design_data):
     }
     return component_task
 
+def gen_comp_task_iter(prompt, design_data, current_component_task):
+    update = {
+        "update_prompt": prompt,
+        "update_description": design_data["description_of_update"],
+        "icons": (
+            [
+                i.lower().replace(" icon", "")
+                for i in design_data["new_component_icons_elements"][
+                    "if_so_what_new_component_icons_elements_are_needed"
+                ]
+            ]
+            if design_data["new_component_icons_elements"][
+                "does_new_component_need_icons_elements"
+            ]
+            else []
+        ),
+        "components": (
+            [
+                {"name": i["library_component_name"], "usage": i["library_component_usage_reason"]} for i in design_data["new_library_components"]["if_so_what_library_components_are_needed"]
+            ]
+            if design_data["new_library_components"]["does_update_need_new_library_components"]
+            else []
+        ),
+        "wireframe": design_data["wireframe_need_to_be_updated"]
+    }
+    component_task = current_component_task
+    component_task["update"] = update
+    return component_task
+
 
 def parse_task(component_task):
     design_task = {
@@ -72,10 +101,7 @@ def parse_task(component_task):
     components_retrieved = [x for i in components_retrieved for x in i] #flatten
 
     retrieved = {
-        "icons": {
-            "icons": [rag_icon(i) for i in design_task["icons"]],
-            "import": "lucide-react",
-        },
+        "icons": [rag_icon(i) for i in design_task["icons"]],
         "components": [i for n, i in enumerate(components_retrieved) if i not in components_retrieved[:n]]
     }
 
@@ -84,13 +110,13 @@ def parse_task(component_task):
     ]
 
     total_suggestion = len(flat_comp_list)
-    suggestion_comp_block = "\n\n".join(
+    suggestion_comp_block_ = "\n\n".join(
         [
-            f"Suggested library component ({i}/{total_suggestion}) : {x[1]['name']} - {x[1]['description']}\n"
+            f"# Suggested library component ({i}/{total_suggestion}) : {x[1]['name']}\n- {x[1]['description']}\n"
             # + f"Suggested usage : {design_task['components'][x[0]]['usage']}\n\n\n"
-            + f"# {x[1]['name']} can be imported into the new component like this:\n"
-            + f"```tsx\n{x[1]['docs']['import'].strip()}\n```\n\n---\n\n# examples of how {x[1]['name']} can be used inside the new component:\n"
-            + f"```tsx\n{x[1]['docs']['use']}```\n\n---"
+            + f"## {x[1]['name']} can be imported into the new component like this:\n"
+            + f"```tsx\n{x[1]['docs']['import'].strip()}\n```\n\n---\n\n## examples of how `{x[1]['name']}` can be used inside the new component:\n"
+            + f"```tsx\n{x[1]['docs']['use']}\n```\n"
             + example_block(
                 x[1]["name"],
                 x[1]["docs"]["examples"],
@@ -100,20 +126,20 @@ def parse_task(component_task):
         ]
     )
 
-    suggestion_comp_block = suggestion_comp_block + "\n\nSuggested library component usages: \n-" + "\n- ".join([f"{i['name']} - {i['usage']}" for i in design_task["components"]])
+    suggestion_comp_block = "# Suggested library component usages: \n- " + "\n- ".join([f"{i['name']} - {i['usage']}" for i in design_task["components"]]) + "\n\n" + suggestion_comp_block_
     if component_task["icons"]:
-        example_icon = retrieved["icons"]["icons"][0]["retrieved"][0]
+        example_icon = retrieved["icons"][0]["retrieved"][0]
         suggestion_icon_block = (
             "Icon elements can optionally be used when making the React component.\n\n---\n\n"
             + "# example: importing icons in the component (only import the ones you need) :\n\n```tsx\nimport { "
             + " , ".join(
-                [" , ".join(x["retrieved"]) for x in retrieved["icons"]["icons"]]
+                [" , ".join(x["retrieved"]) for x in retrieved["icons"]]
             )
             + " } from @govtechmy/myds-react/icon\n```\n\n# example: using an icon inside the component :\n\n```tsx\n"
             + f'<{example_icon} className="h-4 w-4" />\n```\n\n---\n\n\n'
             + "Here are some available icons that might be relevant to the component you are making. You can choose from them if relevant :\n\n```\n"
             + "\n".join(
-                [f"- {i}" for x in retrieved["icons"]["icons"] for i in x["retrieved"]]
+                [f"- {i}" for x in retrieved["icons"] for i in x["retrieved"]]
             )
             + "\n```"
         )
@@ -122,8 +148,8 @@ def parse_task(component_task):
     return suggestion_comp_block, suggestion_icon_block
 
 
-def generate(prompt, design_data, wireframe):
-    component_task = gen_comp_task(prompt, design_data)
+def generate(component_task, wireframe):
+    # component_task = gen_comp_task(prompt, design_data)
     suggestion_comp_block, suggestion_icon_block = parse_task(component_task)
 
     with (
@@ -144,19 +170,123 @@ def generate(prompt, design_data, wireframe):
 
     design_block = f"**When creating components you are to adhere to the Malaysian Design System**\nKeeep in mind components created should have neat and organized layout !\n\n{design_text}"
 
-    build_context = f"""Library components to be used while making the new React component
+    build_context = f"""# Wireframe Design of the component
 
-    {suggestion_comp_block}
+**Adhere fully to the wireframe, crafted by an expert UIUX Designer when building the component !**
 
-    {suggestion_icon_block}
+{wireframe}
 
-    {design_block}
+---
+**Library components to be used while making the new React component.**
 
-    # Component Wireframe:
+{suggestion_comp_block}
 
-    {wireframe}
+{suggestion_icon_block}
 
-    Adhere to the wireframe when creating the component!
-    """
+{design_block}
+"""
 
-    return component_task, build_context
+    return build_context
+
+
+def parse_task_iter(component_task):
+    design_task = {
+        "components": component_task["update"]["components"],
+        "icons": component_task["update"]["icons"],
+    }
+
+    if design_task["components"]: 
+        components_retrieved = [
+                rag_component(i["name"]) for i in design_task["components"]
+            ]
+        
+        components_retrieved = [x for i in components_retrieved for x in i] #flatten
+    else:
+        components_retrieved = None
+
+    retrieved = {
+        "icons": [rag_icon(i) for i in design_task["icons"]] if design_task["icons"] else [],
+        "components": [i for n, i in enumerate(components_retrieved) if i not in components_retrieved[:n]] if components_retrieved else []
+    }
+
+    if retrieved["components"]:
+        flat_comp_list = [
+            (i, x) for i, x in enumerate(retrieved["components"])
+        ]
+
+        total_suggestion = len(flat_comp_list)
+        suggestion_comp_block_ = "\n\n".join(
+            [
+                f"# Suggested library component ({i}/{total_suggestion}) : {x[1]['name']}\n- {x[1]['description']}\n"
+                # + f"Suggested usage : {design_task['components'][x[0]]['usage']}\n\n\n"
+                + f"## {x[1]['name']} can be imported into the new component like this:\n"
+                + f"```tsx\n{x[1]['docs']['import'].strip()}\n```\n\n---\n\n## examples of how `{x[1]['name']}` can be used inside the new component:\n"
+                + f"```tsx\n{x[1]['docs']['use']}\n```\n"
+                + example_block(
+                    x[1]["name"],
+                    x[1]["docs"]["examples"],
+                    x[1]["docs"]["props"],
+                )
+                for i, x in enumerate(flat_comp_list)
+            ]
+        )
+
+        suggestion_comp_block = "# Suggested library component usages: \n- " + "\n- ".join([f"{i['name']} - {i['usage']}" for i in design_task["components"]]) + "\n\n" + suggestion_comp_block_
+    else:
+        suggestion_comp_block = ""
+
+    if component_task["update"]["icons"]:
+        example_icon = retrieved["icons"][0]["retrieved"][0]
+        suggestion_icon_block = (
+            "Icon elements can optionally be used when making the React component.\n\n---\n\n"
+            + "# example: importing icons in the component (only import the ones you need) :\n\n```tsx\nimport { "
+            + " , ".join(
+                [" , ".join(x["retrieved"]) for x in retrieved["icons"]]
+            )
+            + " } from @govtechmy/myds-react/icon\n```\n\n# example: using an icon inside the component :\n\n```tsx\n"
+            + f'<{example_icon} className="h-4 w-4" />\n```\n\n---\n\n\n'
+            + "Here are some available icons that might be relevant to the component you are making. You can choose from them if relevant :\n\n```\n"
+            + "\n".join(
+                [f"- {i}" for x in retrieved["icons"] for i in x["retrieved"]]
+            )
+            + "\n```"
+        )
+    else:
+        suggestion_icon_block = ""
+
+    return suggestion_comp_block, suggestion_icon_block
+
+def generate_iter(component_task, wireframe):
+    # component_task = gen_comp_task(prompt, design_data)
+    suggestion_comp_block, suggestion_icon_block = parse_task_iter(component_task)
+
+    with (
+        open("data/foundation/design_doc_min.md") as design_raw
+    ):  # a distilled version of https://myds.vercel.app/en/docs/design foundation tabs
+        design_text = design_raw.readlines()
+
+    design_text = "".join(design_text)
+
+    with (
+        open("data/foundation/colour.md") as design_colour
+    ):  # extracted from https://github.com/govtechmy/myds/tree/main/packages/style/styles/theme
+        colour_text = design_colour.readlines()
+
+    colour_text = "".join(colour_text)
+
+    design_text = design_text + "\n\n" + colour_text
+
+    design_block = f"**When creating components you are to adhere to the Malaysian Design System**\nKeeep in mind components created should have neat and organized layout !\n\n{design_text}"
+
+    build_context = f"""# Wireframe Design of the component
+
+**Adhere fully to the wireframe, crafted by an expert UIUX Designer when updating the component !**
+
+{wireframe}
+
+---
+{'**Library components to be used while making the new React component.**' + chr(10) + chr(10) if suggestion_comp_block else ''}{suggestion_comp_block}{chr(10)+chr(10) if suggestion_comp_block else ''}{suggestion_icon_block}{chr(10)+chr(10) if suggestion_icon_block else ''}
+{design_block}
+"""
+
+    return build_context
---
+++ util/stages/design_iterate.py
@@ -0,0 +1,163 @@
+import os
+import json
+from google import genai
+from google.genai import types
+from util.util.output_schema import ComponentIterateSchema, WireframeSchema
+GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+def design_update(prompt, current_code, current_wireframe, current_component_task):
+    with open("data/components/myds.json") as f:
+        data = json.load(f)
+    LIBRARY_COMPONENTS_MAP = [
+        {"name": e["name"], "description": e["description"]} for e in data
+    ]
+
+    system_instruction = """Your task is to modify a React component for a web app, according to the user's request.
+If you judge it is relevant to do so, you can specify pre-made library components to use in the component update.
+You must also specify the use of icons if you see that the user's update request requires it."""
+
+    generation_config = types.GenerateContentConfig(
+        temperature=0.6,
+        systemInstruction=system_instruction,
+        responseMimeType="application/json",
+        responseSchema=ComponentIterateSchema,
+    )
+
+
+    contents = [
+        types.Content(
+            parts=[
+                types.Part.from_text(
+                    text="Multiple library components can be used while creating a new component update in order to help you do a better design job, faster.\n"
+                    + "Be creative and only utilize the limited set of components to build the component.\n\n"
+                    + "AVAILABLE LIBRARY COMPONENTS:\n\n"
+                    + "\n".join(
+                        [
+                            f"{i['name']} : {i['description']}"
+                            for i in LIBRARY_COMPONENTS_MAP
+                        ]
+                    )
+                    + "\n\nWhen suggesting icons, use common icon names used in Lucide icon library."
+                    + "\n**IMPORTANT:**"
+                    + "\n**Use CrossIcon when the cross or closing or cancel icon is needed.**"
+                    + "\n**Card component is not available.**"
+                )
+            ],
+            role="model",
+        ),
+        types.Content(
+            parts=[
+                types.Part.from_text(
+                    text=f"- COMPONENT NAME : {current_component_task['name']}\n\n"
+                    + "- COMPONENT DESCRIPTION :\n```\n"
+                    + current_component_task["description"]["user"]
+                    + "\n```\n\n"
+                    + "- additional component suggestions :\n```\n"
+                    + current_component_task["description"]["llm"]
+                    + "\n```\n\n"
+                    + "- NEW COMPONENT UPDATES QUERY :\n```\n"
+                    + prompt
+                    + "\n```\n\n"
+                    + "- current component code :\n```tsx\n"
+                    + current_code
+                    + "\n```\n\n"
+                    + "- current wireframe :\n"
+                    + current_wireframe
+                    + "\n```\n\n"
+                    + "Design the React web component updates for the user, and determine if the wireframe should be updated."
+                    + "When web components are added or removed, the wireframe should be updated"
+                )
+            ],
+            role="user",
+        ),
+    ]
+    design_response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        config=generation_config,
+        contents=contents,
+    )
+    design_data = json.loads(design_response.text)
+    return design_data
+
+def design_layout(component_task, current_wireframe):
+    system_instruction = """You are an expert senior UIUX Designer.\nYour task is to update the wireframe of new React component for a web app, according to the provided task details.\nSpecify the library components and the icons in the wireframe diagram."""
+
+    generation_config = types.GenerateContentConfig(
+        temperature=0.3,
+        systemInstruction=system_instruction,
+        responseMimeType="application/json",
+        responseSchema=WireframeSchema,
+    )
+    print([
+            f"- COMPONENT NAME : {component_task['name']}\n\n"
+            + "- COMPONENT DESCRIPTION :\n```\n"
+            + component_task["description"]["user"]
+            + "\n```\n\n"
+            + "- additional component suggestions :\n```\n"
+            + component_task["description"]["llm"]
+            + "\n```\n\n"
+            + "- COMPONENT UPDATE QUERY :\n```\n"
+            + component_task["update"]["update_prompt"]
+            + "\n```\n\n"
+            + "- additional component update suggestions :\n```\n"
+            + component_task["update"]["update_description"]
+            + "\n```\n\n"
+            + "- current wireframe :\n"
+            + current_wireframe
+            + "\n\n"
+            + "\n\nUpdate the given ASCII wireframe based on the updated design task and available library components.\n"
+            + "The updated wireframe should onlu include changes described by the new updates query\n\n"
+            + "Retain as much of the original wireframe as possible !\n"
+            + "ONLY ADD OR REMOVE ITEMS THAT WERE MENTIONED IN THE UPDATE QUERIES !\n"
+            + "**Library components to be included !**\n- "
+            + "\n- ".join([i["name"] for i in component_task["update"]["components"]])
+            + ("**Icon Elements**\n- " if component_task["update"]["icons"] else "")
+            + (
+                "\n- ".join([i for i in component_task["update"]["icons"]])
+                if component_task["update"]["icons"]
+                else ""
+            )
+            + "\n\nOutput the generated wireframe in a ```ascii ``` block"
+            # + "Specify the library components and the icons in the detailed wireframe diagram.\n"
+            + "The detailed wireframe must look clean and professional as the creative genius you are."
+        ])
+    design_response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        config=generation_config,
+        contents=[
+            f"- COMPONENT NAME : {component_task['name']}\n\n"
+            + "- COMPONENT DESCRIPTION :\n```\n"
+            + component_task["description"]["user"]
+            + "\n```\n\n"
+            + "- additional component suggestions :\n```\n"
+            + component_task["description"]["llm"]
+            + "\n```\n\n"
+            + "- COMPONENT UPDATE QUERY :\n```\n"
+            + component_task["update"]["update_prompt"]
+            + "\n```\n\n"
+            + "- additional component update suggestions :\n```\n"
+            + component_task["update"]["update_description"]
+            + "\n```\n\n"
+            + "- current wireframe :\n"
+            + current_wireframe
+            + "\n\n"
+            + "\n\nUpdate the given ASCII wireframe based on the updated design task and available library components.\n"
+            + "The updated wireframe should onlu include changes described by the new updates query\n\n"
+            + "Retain as much of the original wireframe as possible !\n"
+            + "ONLY ADD OR REMOVE ITEMS THAT WERE MENTIONED IN THE UPDATE QUERIES !\n\n"
+            + "**Library components to be included !**\n- "
+            + "\n- ".join([i["name"] for i in component_task["update"]["components"]])
+            + ("**Icon Elements**\n- " if component_task["update"]["icons"] else "")
+            + (
+                "\n- ".join([i for i in component_task["update"]["icons"]])
+                if component_task["update"]["icons"]
+                else ""
+            )
+            + "\n\nOutput the generated wireframe in a ```ascii ``` block"
+            # + "Specify the library components and the icons in the detailed wireframe diagram.\n"
+            + "The detailed wireframe must look clean and professional."
+        ],
+    )
+    design_response = json.loads(design_response.text)["ascii_wireframe"]
+    return design_response
