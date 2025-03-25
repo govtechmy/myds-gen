59ab1ff478e8ef9e74362da0dab6ac2679c521e0
justintzeji <justintzeji@gmail.com>
2025-03-25 21:55:13 +0800

    chore: improving prompts

--- src/util/output_schema.py
+++ src/util/output_schema.py
@@ -37,13 +37,25 @@ class LibraryComponentIterate(BaseModel):
     does_update_need_new_library_components: bool
     if_so_what_library_components_are_needed: Optional[List[LibraryComponent]] = None
 
-
+class IterStep(BaseModel):
+    step_num: int
+    name: str
+    detailed_instructions: str
 class ComponentIterateSchema(BaseModel):
     description_of_update: str
     new_component_icons_elements: NewComponentIconsElements
     new_library_components: LibraryComponentIterate
     wireframe_need_to_be_updated: bool
+    steps: List[IterStep]
 
 
 class TsxOutput(BaseModel):
     tsx: str
+
+class PromptImproved(BaseModel):
+    improved_request: str
+    components: List[LibraryComponent]
+
+class PromptImprovedIter(BaseModel):
+    improved_request: str
+    additional_components: Optional[List[LibraryComponent]]
